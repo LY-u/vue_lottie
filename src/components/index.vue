c7a99b5a68201b4fa7ddfe63160ae7389e46fdc6
LY-u <18868801547@163.com>
2018-05-24 18:31:47 +0800

    ..

---
+++ src/components/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <lottie :options="defaultOptions" :height="100" :width="200" @animCreated="handleAnimation"/>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Lottie from './lottie';
+  import * as animationData from './loading.json';
+
+export default {
+  components:{
+    'lottie': Lottie,
+  },
+  data () {
+    return {
+      msg:'Welcome to Lottie',
+      defaultOptions: {animationData: animationData},
+    }
+  },
+  methods:{
+    handleAnimation(){},
+  }
+}
+</script>
+
